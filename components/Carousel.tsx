ecb4176ff271c39bb2552fae8e410d2b356e16c4
praveenNagaraj <praveenacchusn123@gmail.com>
2021-09-19 22:12:34 +0530

    reduce images array

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -10,20 +10,21 @@ import React from 'react';
 
 const CarouselHome = () => {
   return (
-    <MDBContainer className="slider-home-banner carousel slide">
+    <MDBContainer className="slider-home-banner carousel">
       <MDBCarousel
         activeItem={1}
-        length={8}
+        length={3}
         showControls={true}
         showIndicators={true}
         className="z-depth-1"
         id="carousel1_indicator"
+        slide
       >
         <MDBCarouselInner>
           <MDBCarouselItem itemId="1">
             <MDBView>
               <img
-                className="d-block w-100"
+                className="d-block w-100 img-fluid"
                 src="images/demo/Banners/b1.jpg"
                 alt="First slide"
               />
@@ -32,7 +33,7 @@ const CarouselHome = () => {
           <MDBCarouselItem itemId="2">
             <MDBView>
               <img
-                className="d-block w-100"
+                className="d-block w-100 img-fluid"
                 src="images/demo/Banners/b2.jpg"
                 alt="Second slide"
               />
@@ -41,7 +42,7 @@ const CarouselHome = () => {
           <MDBCarouselItem itemId="3">
             <MDBView>
               <img
-                className="d-block w-100"
+                className="d-block w-100 img-fluid"
                 src="images/demo/Banners/b3.jpg"
                 alt="Third slide"
               />
@@ -50,7 +51,7 @@ const CarouselHome = () => {
           <MDBCarouselItem itemId="4">
             <MDBView>
               <img
-                className="d-block w-100"
+                className="d-block w-100 img-fluid"
                 src="images/demo/Banners/b4.jpg"
                 alt="Third slide"
               />
@@ -59,7 +60,7 @@ const CarouselHome = () => {
           <MDBCarouselItem itemId="5">
             <MDBView>
               <img
-                className="d-block w-100"
+                className="d-block w-100 img-fluid"
                 src="images/demo/Banners/b5.jpg"
                 alt="Third slide"
               />
@@ -68,7 +69,7 @@ const CarouselHome = () => {
           <MDBCarouselItem itemId="6">
             <MDBView>
               <img
-                className="d-block w-100"
+                className="d-block w-100 img-fluid"
                 src="images/demo/Banners/b6.jpg"
                 alt="Third slide"
               />
@@ -77,7 +78,7 @@ const CarouselHome = () => {
           <MDBCarouselItem itemId="7">
             <MDBView>
               <img
-                className="d-block w-100"
+                className="d-block w-100 img-fluid"
                 src="images/demo/Banners/b7.jpg"
                 alt="Third slide"
               />
@@ -86,7 +87,7 @@ const CarouselHome = () => {
           <MDBCarouselItem itemId="8">
             <MDBView>
               <img
-                className="d-block w-100"
+                className="d-block w-100 img-fluid"
                 src="images/demo/Banners/b8.jpg"
                 alt="Third slide"
               />
